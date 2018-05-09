4e43c376a13c72c133631a435a9b6a794b890221
Alison Schmidt <alison.e.schmidt@gmail.com>
2018-05-09 15:26:39 +0800

    finished all models and routes--controllers created but not finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+
+  get 'restaurants/index'
+
+  get 'restaurants/new'
+
+  get 'restaurants/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
+
+
+
+
+  # get "restaurants", to: "restaurants#index"
+
+  # get "resturants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+
+  # get "restaurants/:id" to: "restaurants#show"
+
+  # get "restaurants/:restaurant_id/reviews/new" to: "reviews#new"
+  # post "restaurants/:restaurant_id/reviews" to: "reviews#create"
+
