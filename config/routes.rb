4cc6b4232690a14e8c17f1807afe5a7ca3bd4ebf
Alison Schmidt <alison.e.schmidt@gmail.com>
2018-05-09 16:26:30 +0800

    Finished the challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-
-  get 'restaurants/index'
-
-  get 'restaurants/new'
-
-  get 'restaurants/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :new, :create, :show] do
